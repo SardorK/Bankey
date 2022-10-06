4e3219a0b10dd1340cd2fd8ba7d9d14981284fa5
SardorK <sardorkarshiev6167@gmail.com>
2022-10-06 11:20:02 +0500

    Create a TableView

--- Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class AccountSummaryViewController: UIViewController{
-
+    
+    var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
     }
+}
+
+extension AccountSummaryViewController{
+    func setup(){
+        setupTableView()
+    }
+    
+    func setupTableView(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        ])
+    }
+}
+
+extension AccountSummaryViewController: UITableViewDataSource,UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
 
